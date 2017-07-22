aa72990dae36dae22233634257aaf8c46a4d32e9
prince <prince@DESKTOP-A7O31D0>
2017-07-22 18:17:21 +0530

    commit

--- src/TestCode.java
+++ src/TestCode.java
@@ -1,9 +1,15 @@
 
 public class TestCode {
-
+	private int counter;
 	public static void main(String[] args) {
-		
+	
 
+	}
+	public int getCounter() {
+		return counter;
+	}
+	public void setCounter(int counter) {
+		this.counter = counter;
 	}
 
 }
